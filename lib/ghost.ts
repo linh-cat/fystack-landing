7beb43db2580c88ee50b84957040425f00b69129
anhthii <nguyendanganhthi247@gmail.com>
2026-07-01 08:28:12 +0700

    Fix wrong env

--- lib/ghost.ts
+++ lib/ghost.ts
@@ -2,8 +2,8 @@ import GhostContentAPI from '@tryghost/content-api';
 
 // Initialize Ghost Content API
 const api = new GhostContentAPI({
-  url: process.env.NEXT_PUBLIC_GHOST_URL || 'http://localhost:2368',
-  key: process.env.NEXT_PUBLIC_GHOST_CONTENT_API_KEY as string,
+  url: process.env.GHOST_URL || 'http://localhost:2368',
+  key: process.env.GHOST_CONTENT_API_KEY as string,
   version: 'v5.0'
 });
 
